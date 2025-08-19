9598bdc7b4d5dab6269a26a07613fe847fc279e1
Martin Tale <me@martintale.com>
2025-08-19 21:52:38 +0100

    Comment out EdgeLinkButton instances in index.ts
    The EdgeLinkButton components for Discord and Ko-fi links have been commented out in src/index.ts. This change temporarily removes these external links from the UI, possibly for testing, deployment, or design reasons.

--- src/index.ts
+++ src/index.ts
@@ -29,25 +29,25 @@ window.addEventListener("DOMContentLoaded", () => {
 	mount(bodyElement, gameContainer);
 	createLivesContainer(gameContainer);
 	createGameOverScreen(gameContainer);
-  const screen = titleScreen(() => {
-    screen.remove();
-    state.lives.value = state.maxLives.value;
-    initGame();
-    startGameLoop();
-  });
-  mount(gameContainer, screen);
+	const screen = titleScreen(() => {
+		screen.remove();
+		state.lives.value = state.maxLives.value;
+		initGame();
+		startGameLoop();
+	});
+	mount(gameContainer, screen);
 
 	initMusic();
 
-	new EdgeLinkButton(bodyElement, SVGs.discord, "#5865F2", 8, -8, "https://discord.gg/kPf8XwNuZT");
-	new EdgeLinkButton(
-		bodyElement,
-		SVGs.coffee,
-		"#FBAA19",
-		64,
-		-8,
-		"https://ko-fi.com/martintale?ref=js13kgames-template",
-	);
+	// new EdgeLinkButton(bodyElement, SVGs.discord, "#5865F2", 8, -8, "https://discord.gg/kPf8XwNuZT");
+	// new EdgeLinkButton(
+	// 	bodyElement,
+	// 	SVGs.coffee,
+	// 	"#FBAA19",
+	// 	64,
+	// 	-8,
+	// 	"https://ko-fi.com/martintale?ref=js13kgames-template",
+	// );
 
 	soundToggle = new EdgeButton(bodyElement, SVGs.sound, "sound", 8, 8);
 
